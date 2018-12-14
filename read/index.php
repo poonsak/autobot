2e8fa92df8f7546df9cb246a836ff52b3450f52a
Poonsak Chananoi <poonsak.eng@gmail.com>
2018-12-14 19:00:39 +0700

    even add 1st workshop DB

---
+++ read/index.php
@@ -0,0 +1,14 @@
+<?php
+
+                $host='ec2-54-83-197-230.compute-1.amazonaws.com';
+                $dbname='dbujg2s2tqvhqf';
+                $user='egksxrkouiprpf';
+                $pass='d83d5caf218a9961c028cb9a47496c849e2479edd7003182916529828647da3a';
+                $connection=new PDO("pgsql:host=$host;dbname=$dbname",$user , $pass);
+
+                   
+                $result=$connection->query("SELECT * FROM appointments");
+            if($result != null){
+                echo $result->rowCount();
+            }
+
