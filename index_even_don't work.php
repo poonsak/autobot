a307e2709e92ebc56ecac6649fecd540b7d239d2
Poonsak Chananoi <poonsak.eng@gmail.com>
2018-12-14 16:57:41 +0700

    even add firend

--- index_even_don't work.php
+++ index_even_don't work.php
@@ -7,8 +7,6 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
-
-//Token
 $channel_token ='mgfm1Bp+N7dJOqZP59o2t3n42y9hZ/S/gpbP4/XPSUFFRCsyiHwdxiyOQBoiBjPmKBC+hgF1X+6axLs88EHCpPSsqp4o+iv8qVameG09UuF1mEFmE118ZuNNXIJvLR25+BwC6RRTtUHwqR9fyPRIPgdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '608522996c2e2cb13d29b6a1dd1cb09f';
 
@@ -22,21 +20,32 @@ if(!is_null($events['events'])){
     foreach($events['events']as $event){
     
         //Line API send a lot of event type , we interested un message only.
-        if($event['type']=='follow'){
+        if($event['type']=='message'){
 
              //Get replyToken
              $replyToken=$event['replyToken'];
             //  $userId = $event['source']['userId'];
              
-            //creating
-            $respMessage='Thank you. I try yo be you best friend';
+            switch($event['message']['type']){
+
+                case 'file':
+                    $messageID=$event['message']['id'];
+                    $fileName=$event['message']['fileName'];
+                    
+                    //Reply message
+                    $respMessage='Hello, your UserID is ' .$messageID.' and file name is ' .$fileName;
+                break;
+
+                default:
+                     $respMessage='Please send file only';
+                break;
 
 
 
             }
             $httpClient=new CurlHTTPClient($channel_token);
             $bot=new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-
+            
             $textMessageBuilder=new TextMessageBuilder($respMessage);
             $response=$bot->replyMessage($replyToken,$textMessageBuilder);
         }
